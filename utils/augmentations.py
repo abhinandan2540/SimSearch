283e323ab60c5c88af6da7a79ae1fff129411201
Abhinandan Mandal <105782385+abhinandan2540@users.noreply.github.com>
2026-03-09 01:47:56 +0530

    augmentations

---
+++ utils/augmentations.py
@@ -0,0 +1,26 @@
+
+# self supervised learning uses two views of the same image
+# creating two images using augmentation techniques such as
+# random crop, color jitter, flip, blur, rotation etc
+
+
+# img transformation pre self-supervised learning
+import torchvision.transforms as T
+
+# img transform taken from SimCLR
+SimSearch_transform = T.Compose([
+    T.RandomResizedCrop(224),  # img size 224 pxls
+    T.RandomHorizontalFlip(),  # horizontal flip
+    T.RandomApply([
+        T.ColorJitter(  # controlling brightness, saturation, heu
+            brightness=0.4,
+            contrast=0.4,
+            saturation=0.4,
+            hue=0.1
+        )
+    ], p=0.8),
+    T.RandomGrayscale(p=0.2),  # grayscaling image
+    T.GaussianBlur(kernel_size=23),  # blurring img
+    T.ToTensor()  # transforming img into tensor format
+])
+
