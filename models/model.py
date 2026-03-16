2b9becc0b9bc65e2d8525c7d0334c28d5509869c
Abhinandan Mandal <105782385+abhinandan2540@users.noreply.github.com>
2026-03-17 02:17:11 +0530

    simsearch-model

---
+++ models/model.py
@@ -0,0 +1,35 @@
+
+# constrastive learning required a projection head 
+# for proper model training
+
+import torch.nn as nn 
+from .encoder import SimSearchEncoder
+
+class SimSearchModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.encoder=SimSearchEncoder()
+
+        # self.projector=nn.Sequential(
+        #     nn.Linear(256,256), # 256 embedding vector representation
+        #     nn.ReLU(), # activation function for contrastive learning to get better representation
+        #     nn.Linear(256,256) # output
+
+        # )
+        
+        # upgrade for stabilized training, prevent representation collaspe, improving feature quality
+        self.projector = nn.Sequential(
+            nn.Linear(256,512),
+            nn.BatchNorm1d(512),
+            nn.ReLU(),
+            nn.Linear(512,256)
+        )
+
+    def forward(self,x):
+        features=self.encoder(x) # from img -> feature representations
+        projections=self.projector(features) # feature representations -> embed projections
+        return projections
+    
+
+
+    
